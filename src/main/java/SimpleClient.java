0069db2e7eb0d2f7ce2b79db48bf0b426c2d2077
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-12 21:17:16 +0200

    Add classes #2

---
+++ src/main/java/SimpleClient.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SimpleClient
+{
+    static void main(String[] args)
+    {
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 5678;
+
+        try(Socket client = new Socket("localhost", port))
+        {
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            String request, response;
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println(client);
+
+            while(true)
+            {
+                System.out.print("Enter message: ");
+                request = scanner.nextLine();
+
+                if(request.equalsIgnoreCase("end"))
+                {
+                    break;
+                }
+
+                out.println(request);
+
+                response = in.readLine();
+                System.out.println("Server response: " + response);
+            }
+
+        }
+        catch (IOException e)
+        {
+            System.out.println("Client exception: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/SimpleServer.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer
+{
+    static void main(String[] args)
+    {
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 5678;
+
+        try(ServerSocket server = new ServerSocket(port))
+        {
+            boolean isServerOn = true;
+            while (isServerOn)
+            {
+                try (Socket socket = server.accept())
+                {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    String request;
+
+                    System.out.println("Client #" + socket.getPort() + " connected...");
+
+                    while(true)
+                    {
+                        request = in.readLine();
+
+                        if(request.equalsIgnoreCase("end")) {
+                            break;
+                        } else if(request.equalsIgnoreCase("end-server")) {
+                            System.out.println("Shutting down the server...");
+                            isServerOn = false;
+                            break;
+                        }
+
+                        System.out.println("Server to client #" + socket.getPort() + ": " + request);
+                        out.println("Server-" + request);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Client ends connection...");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e.getMessage());
+        }
+    }
+}
